edae8e20dc984eb3786e1db0235e875d0583c3be
Petter Näslund <pettern@superoffice.com>
2021-09-13 09:08:16 +0300

    Renamed some things

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace L002B_Inl2_PetterNaslund
+namespace CheckSalesPersonsPerLevel
 {
     class Program
     {
